29ee3c1b210804fae14770663a214569e4077d5d
ysjkdssj <ys2110119@y-y.hs.kr>
2022-12-07 14:54:34 +0900

    좌표
    지도에 표시할 주소와 좌표 넣기

--- back/backend/src/main/webapp/ Query.sql
+++ back/backend/src/main/webapp/ Query.sql
@@ -16,7 +16,12 @@ CREATE TABLE place_tbl_map (
     place_facilities CHAR(1) -- 1 : 음식점, 2 : 마트, 3 : 놀거리, 4 : 편의시설
 );
 
-insert into place_tbl_map values('031-704-0704', '효자촌 미래타운 수제 돈까스', '37.373058', '127.137566', '1');
+select * from place_tbl_map;
+
+insert into place_tbl_map values('031-704-0704', '효자촌 미래타운 수제돈까스', ' 경기도 성남시 분당구 서현2동 307', '37.373058', '127.137566', '1');
+insert into place_tbl_map values('000-000-0000', '국시 엔 국수', ' 경기도 성남시 분당구 서현동 312-3', ' 37.371743', '127.139374', '1');
+insert into place_tbl_map values('031-729-9400', '청소년 수련관', '경기도 성남시 불정로386번길 38', '37.372176', '127.139918', '4')
+
 
 /* 즐겨찾기 */
 CREATE TABLE favorites_tbl_map ( -- 누가 어느 가게에 즐겨 찾기를 하였는지
