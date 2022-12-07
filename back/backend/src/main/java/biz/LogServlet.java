f86d37553e32a3ae19535739f1f8418a10a7691e
ysjkdssj <ys2110119@y-y.hs.kr>
2022-12-07 19:10:20 +0900

    로그인 구현
    로그인 작성중

--- back/backend/src/main/java/biz/LogServlet.java
+++ back/backend/src/main/java/biz/LogServlet.java
@@ -1,41 +1,48 @@
 package biz;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class LogServlet
- */
-@WebServlet("/LogServlet")
-public class LogServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LogServlet() {
+import dao.MemberDAO;
+import vo.MemberVO;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public LoginServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        String id, pwd;
+        MemberVO vo = null;
+        PrintWriter out = response.getWriter();
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+        id = request.getParameter("id");
+        pwd = request.getParameter("pwd");
 
+        MemberDAO dao = new MemberDAO();
+        System.out.println(id);
+        vo = dao.getMemberData(id);
+
+
+        if(vo == null || !pwd.equals(vo.getMemberPwd())) {
+            out.println("<script> alert('회원정보가 맞지 않습니다'); history.back(); </script>");
+        } else {
+            HttpSession session = request.getSession();
+            session.setAttribute("loginOK", vo);
+            response.sendRedirect("/main.jsp");
+        }
+    }
 }
+
