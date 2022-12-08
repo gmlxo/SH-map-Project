8c915f86e56543c63dd270daabc6ade3878a2a54
ysjkdssj <ys2110119@y-y.hs.kr>
2022-12-08 10:48:19 +0900

    .

--- back/backend/src/main/java/dao/UserDAO.java
+++ back/backend/src/main/java/dao/UserDAO.java
@@ -6,37 +6,102 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import common.JDBCUtil;
-import vo.UserVO;
+import vo.MemberVO;
 
 public class UserDAO {
-	Connection conn = null;
-	PreparedStatement pstmt = null;
-	ResultSet rs = null;
-	String sql;
-
-	public UserVO login(String id) {
-		UserVO vo = null;
-
-		sql = "select * from user_tbl_map where user_id = ?";
-
+	public UserDAO() {
+	}
+	
+	public UserDAO getUserData(String id) {
+		UserDAO vo = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT * FROM member WHERE member_id=?";
+		
 		try {
 			conn = JDBCUtil.getConnection();
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
-
-			if (rs.next()) {
+			
+			if(rs.next()) {
 				vo = new UserVO();
-				vo.setUser_id(rs.getString("user_id"));
-				vo.setUser_Phone_number(rs.getString("user_profile"));
-				vo.setUser_password(rs.getString("user_password"));
-				vo.setUser_Phone_number(rs.getString("user_Phone_number"));
+				vo.setuser_id(rs.getString("user_id"));
+				vo.setuser_password(rs.getString("user_password"));
+				vo.setuser_Phone_number(rs.getString("user_Phone_number"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+			System.out.println("checkLoginUser : sql error");
 		} finally {
 			JDBCUtil.close(conn, pstmt, rs);
 		}
+		
 		return vo;
+	}
+	
+	public boolean existID(String id) {
+		boolean isExist = false;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT * FROM member WHERE member_id=?";
+		
+		try {
+			conn = JDBCUtil.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				isExist = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("checkLoginUser : sql error");
+		} finally {
+			JDBCUtil.close(conn, pstmt, rs);
+		}
+		
+		return isExist;
+	}
+	
+	public int removeMember(String id) {
+		int result = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "DELETE FROM member WHERE member_id=?";
+		
+		try {
+			conn = JDBCUtil.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public int insertMember(UserVO data) {
+		int result = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "INSERT INTO member(member_id, member_pwd) values(?,?)";
+		
+		try {
+			conn = JDBCUtil.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, data.getMemberId());
+			pstmt.setString(2, data.getMemberPwd());
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 }
