31bd7e737880a398f3be15731e47409276bb172d
gmlxo <msaaa794274@gmail.com>
2022-12-29 14:32:54 +0900

    Update map.js

--- front/js/map.js
+++ front/js/map.js
@@ -2,45 +2,55 @@ window.onload = function map() {
     var container = document.getElementById('map');
     var options = {
         // 기본 좌표 설정 _ 양영디지털고등학교 
-        // center: new kakao.maps.LatLng(37.373583, 127.140544),
-        center: new kakao.maps.LatLng(37.373058, 127.137566),
+        center: new kakao.maps.LatLng(37.373583, 127.140544),
+        //center: new kakao.maps.LatLng(37.373058, 127.137566),
         level: 2
     };
 
-    var map = new kakao.maps.Map(container, options);
-}
+    var map_123 = new kakao.maps.Map(container, options);
 
-function getPoints() {
-    var gubun = document.getElementById("facilities").options[document.getElementById("facilities").selectedIndex].value;
-    var points;
-    if (gubun == "1") {
-        points = [
-            new kakao.maps.LatLng(37.373058, 127.137566)
-        ]
+    // id가 facilities 인 값을 받아온다
+    var guabunSelect = document.getElementById("facilities").options[document.getElementById("facilities").selectedIndex].value;
+    var positions;
+
+    if (guabunSelect != "0") {
+        // facilities 가 1_음식점 일시 
+        if (guabunSelect == "1") {
+            // 마커를 표시할 위치와 title 객체 배열입니다
+
+            positions = [
+                {
+                    title: '효자촌 미래타운 수제돈까스',
+                    latlng: new kakao.maps.LatLng(37.373058, 127.137566)
+                },
+                {
+                    title: '국시 엔 국수',
+                    latlng: new kakao.maps.LatLng(37.371743, 127.139374)
+                },
+                {
+                    title: '앙토낭 카렘',
+                    latlng: new kakao.maps.LatLng(37.373933, 127.136649)
+                }
+            ];
+        }
+        // 마커 이미지의 이미지 주소입니다
+        var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
+        for (var i = 0; i < positions.length; i++) {
+
+            // 마커 이미지의 이미지 크기 입니다
+            var imageSize = new kakao.maps.Size(24, 35);
+
+            // 마커 이미지를 생성합니다    
+            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+            // 마커를 생성합니다
+            var marker = new kakao.maps.Marker({
+                map: map.map_123, // 마커를 표시할 지도
+                position: positions[i].latlng, // 마커를 표시할 위치
+                title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                image: markerImage // 마커 이미지 
+            });
+        }
     }
-    console.log(points);
-    setPoint(points);
-}
-
-function setPoint(points) {
-    map();
-    // 지도를 재설정할 범위정보를 가지고 있을 LatLngBounds 객체를 생성합니다
-    var bounds = new kakao.maps.LatLngBounds();
-
-    var i, marker;
-
-    for (i = 0; i < points.length; i++) {
-        // 배열의 좌표들이 잘 보이게 마커를 지도에 추가합니다
-        marker = new kakao.maps.Marker({ position: points[i] });
-        marker.setMap(map);
-
-        // LatLngBounds 객체에 좌표를 추가합니다
-        bounds.extend(points[i]);
-    }
-}
-
-function setBounds() {
-    // LatLngBounds 객체에 추가된 좌표들을 기준으로 지도의 범위를 재설정합니다
-    // 이때 지도의 중심좌표와 레벨이 변경될 수 있습니다
-    map.setBounds(bounds);
 }
