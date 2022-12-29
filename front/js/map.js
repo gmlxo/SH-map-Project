d894b92226d46b1d8a03a69efa1f56945264b6a6
gmlxo <msaaa794274@gmail.com>
2022-12-29 15:02:19 +0900

    .

--- front/js/map.js
+++ front/js/map.js
@@ -6,7 +6,9 @@ window.onload = function map() {
         //center: new kakao.maps.LatLng(37.373058, 127.137566),
         level: 2
     };
+}
 
+function setra(){
     var map_123 = new kakao.maps.Map(container, options);
 
     // id가 facilities 인 값을 받아온다
@@ -36,21 +38,23 @@ window.onload = function map() {
         // 마커 이미지의 이미지 주소입니다
         var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
-        for (var i = 0; i < positions.length; i++) {
+        if(positions != null) {
+            for (var i = 0; i < positions.length; i++) {
 
-            // 마커 이미지의 이미지 크기 입니다
-            var imageSize = new kakao.maps.Size(24, 35);
-
-            // 마커 이미지를 생성합니다    
-            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
-            // 마커를 생성합니다
-            var marker = new kakao.maps.Marker({
-                map: map.map_123, // 마커를 표시할 지도
-                position: positions[i].latlng, // 마커를 표시할 위치
-                title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                image: markerImage // 마커 이미지 
-            });
+                // 마커 이미지의 이미지 크기 입니다
+                var imageSize = new kakao.maps.Size(24, 35);
+    
+                // 마커 이미지를 생성합니다    
+                var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+    
+                // 마커를 생성합니다
+                var marker = new kakao.maps.Marker({
+                    map: map_123, // 마커를 표시할 지도
+                    position: positions[i].latlng, // 마커를 표시할 위치
+                    title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                    image: markerImage // 마커 이미지 
+                });
+            }
         }
     }
 }
