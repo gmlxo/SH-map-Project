dda5a2b73e63b350503aa4c727ce90024ab28973
gmlxo <msaaa794274@gmail.com>
2022-12-29 16:27:12 +0900

    .

--- front/js/map.js
+++ front/js/map.js
@@ -1,16 +1,17 @@
 window.onload = function map() {
-    var container = document.getElementById('map');
-    var options = {
-        // 기본 좌표 설정 _ 양영디지털고등학교 
-        center: new kakao.maps.LatLng(37.373583, 127.140544),
-        //center: new kakao.maps.LatLng(37.373058, 127.137566),
-        level: 2
-    };
+    var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
+        mapOption = { 
+            center: new kakao.maps.LatLng(37.373583, 127.140544),
+            level: 3 // 지도의 확대 레벨
+        };
+
+    console.log(mapContainer)
+    console.log(mapOption)
+    
+    let map = new kakao.maps.Map(mapContainer, mapOption);
 }
 
-function setra(){
-    var map_123 = new kakao.maps.Map(container, options);
-
+function setMarkers(){
     // id가 facilities 인 값을 받아온다
     var guabunSelect = document.getElementById("facilities").options[document.getElementById("facilities").selectedIndex].value;
     var positions;
@@ -35,6 +36,7 @@ function setra(){
                 }
             ];
         }
+        alert(map)
         // 마커 이미지의 이미지 주소입니다
         var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
@@ -48,12 +50,13 @@ function setra(){
                 var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
     
                 // 마커를 생성합니다
-                var marker = new kakao.maps.Marker({
-                    map: map_123, // 마커를 표시할 지도
+                marker = new kakao.maps.Marker({
+                    map: map, // 마커를 표시할 지도
                     position: positions[i].latlng, // 마커를 표시할 위치
                     title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
                     image: markerImage // 마커 이미지 
                 });
+
             }
         }
     }
--- front/js/test.js
+++ front/js/test.js
@@ -1,36 +1,47 @@
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
+    mapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
 
-
-function getPoints() {
-    var gubun1 = document.getElementById("facilities").options[document.getElementById("facilities").selectedIndex].value;
-    var points1;
-    if (gubun1 == "1") {
-        points1 = [
-            new kakao.maps.LatLng(37.373058, 127.137566)
-        ]
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+ 
+// 마커를 표시할 위치와 title 객체 배열입니다 
+var positions = [
+    {
+        title: '카카오', 
+        latlng: new kakao.maps.LatLng(33.450705, 126.570677)
+    },
+    {
+        title: '생태연못', 
+        latlng: new kakao.maps.LatLng(33.450936, 126.569477)
+    },
+    {
+        title: '텃밭', 
+        latlng: new kakao.maps.LatLng(33.450879, 126.569940)
+    },
+    {
+        title: '근린공원',
+        latlng: new kakao.maps.LatLng(33.451393, 126.570738)
     }
-    console.log(points1);
-    setPoint(points1);
-}
+];
 
-function setPoint(points1) {
-    map();
-    // 지도를 재설정할 범위정보를 가지고 있을 LatLngBounds 객체를 생성합니다
-    var bounds = new kakao.maps.LatLngBounds();
-
-    var i, marker;
-
-    for (i = 0; i < points.length; i++) {
-        // 배열의 좌표들이 잘 보이게 마커를 지도에 추가합니다
-        marker = new kakao.maps.Marker({ position: points1[i] });
-        marker.setMap(map);
-
-        // LatLngBounds 객체에 좌표를 추가합니다
-        bounds.extend(points1[i]);
-    }
-}
-
-function setBounds() {
-    // LatLngBounds 객체에 추가된 좌표들을 기준으로 지도의 범위를 재설정합니다
-    // 이때 지도의 중심좌표와 레벨이 변경될 수 있습니다
-    map.setBounds(bounds);
+// 마커 이미지의 이미지 주소입니다
+var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+    
+for (var i = 0; i < positions.length; i ++) {
+    
+    // 마커 이미지의 이미지 크기 입니다
+    var imageSize = new kakao.maps.Size(24, 35); 
+    
+    // 마커 이미지를 생성합니다    
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+    
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커를 표시할 위치
+        title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image : markerImage // 마커 이미지 
+    });
 }
